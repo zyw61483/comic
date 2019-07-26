d7438c5025cc972b45883c9901c7d18ab64f0688
zhaoyiwei <zhaoyiwei@zhongan.com>
2019-07-26 16:30:26 +0800

    新增漫画淘资源爬取

---
+++ src/main/java/entity/MhtComic.java
@@ -0,0 +1,79 @@
+package entity;
+
+import com.alibaba.fastjson.JSONArray;
+import com.google.common.collect.Lists;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * MhtComic
+ *
+ * @author: zhaoyiwei
+ * @date: 2019/7/26 10:03
+ */
+@Slf4j
+public class MhtComic extends Comic {
+
+    private static final String chapterIndexReg = "<li>\\s*<a href=\"(.*?)\">\\s*<span class=\"list_con_tb\"></span><span class=\"list_con_zj\">(.*?)\\s*</span><span class=\"list_con_tips\">";
+    private static final String chapterReg = "var chapterImages = (.*?);";
+    private static final String commicNameReg = "<h1><a href=\"(.*?)\">(.*?)</a></h1>";
+    private static final String mht = "https://www.manhuatao.com";
+
+    public MhtComic(String chapterIndexUrl, Integer startChapter, Integer endChapter) throws IOException {
+        super(chapterIndexUrl, startChapter, endChapter);
+    }
+
+    public static void main(String[] args) throws Exception {
+        MhtComic comic = new MhtComic("https://www.manhuatao.com/manhua/5185/", 61, 100);
+        comic.download();
+    }
+
+    @Override
+    public List<ChapterIndex> getChapterIndexUrls(String content) {
+        List<ChapterIndex> result = Lists.newArrayList();
+        String splitReg = "<ul id=\"chapter-list-1\" data-sort=\"asc\" class=\"list_con_li autoHeight\">(.*?)</ul>";
+        Pattern split = Pattern.compile(splitReg, Pattern.MULTILINE | Pattern.DOTALL);
+        Matcher splitMatcher = split.matcher(content);
+        if (splitMatcher.find()) {
+            content = splitMatcher.group(0);
+        }
+        Pattern r = Pattern.compile(chapterIndexReg, Pattern.MULTILINE | Pattern.DOTALL);
+        Matcher m = r.matcher(content);
+        while (m.find()) {
+//            log.info("name:{} url:{}", m.group(2), m.group(1));
+            ChapterIndex index = new ChapterIndex();
+            index.setUrl(mht + m.group(1));
+            index.setName(m.group(2));
+            result.add(index);
+        }
+        return result;
+    }
+
+    @Override
+    public Chapter getChapter(String content, String chapterName) {
+        List<String> result = Lists.newArrayList();
+        Pattern r = Pattern.compile(chapterReg);
+        Matcher m = r.matcher(content);
+        if (m.find()) {
+            List<String> list = (List<String>) JSONArray.parse(m.group(1));
+            for (String temp : list) {
+                result.add("https://restp.dongqiniqin.com/" + temp);
+            }
+        }
+        return Chapter.builder().commicName(getComicName(content)).chapterName(chapterName).picUrls(result).build();
+    }
+
+    private String getComicName(String content) {
+        Pattern r = Pattern.compile(commicNameReg);
+        Matcher m = r.matcher(content);
+        if (m.find()) {
+            return m.group(2);
+        }
+        return "xxx";
+    }
+
+}
